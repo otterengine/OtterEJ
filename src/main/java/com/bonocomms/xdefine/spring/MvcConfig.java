65777e73461f6660cec71f464d6edfc506a6e402
suhanmoon <suhanmoon@fe80:0:0:0:b771:ebb8:475d:ede4%10>
2016-10-28 17:19:43 +0900

    Task Executor Added.

--- src/main/java/com/bonocomms/xdefine/spring/MvcConfig.java
+++ src/main/java/com/bonocomms/xdefine/spring/MvcConfig.java
@@ -1,8 +1,6 @@
 package com.bonocomms.xdefine.spring;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import javax.servlet.ServletContext;
@@ -10,8 +8,6 @@ import javax.servlet.ServletContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.hibernate.SessionFactory;
 import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -20,9 +16,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -40,31 +37,14 @@ import com.bonocomms.xdefine.mail.SMTPMailSender;
 @PropertySource("classpath:xdefine.properties")
 @ComponentScan(basePackages = "${webapp.package}")
 @Import(value = { SpringVelocityConfig.class })
+@EnableScheduling
 public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
-	private Logger logger = LoggerFactory.getLogger("");
 	private ApplicationContext context;
 	
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.context = applicationContext;
-
-		logger.info("  ");
-		logger.info(" #############################################################");
-		logger.info("  ");
-		logger.info(" Welcome to X-Define");
-		logger.info(" (c) Copyright Bono Communications Inc, Korea");
-		logger.info(" ");
-		logger.info(" Using the Tool can work more convenient.");
-		logger.info(" Detailed information on the framework, please refer to the site.");
-		logger.info(" Thank you.");
-		logger.info(" ");
-		logger.info(" http://www.xdefine.net");
-		logger.info(" ");
-		logger.info(" ");
-		logger.info(" #############################################################");
-		logger.info(" ");
-		
 	}
 	
 	@Bean
@@ -76,40 +56,14 @@ public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 	public CommonsMultipartResolver multipartResolver() {
 		return new CommonsMultipartResolver();
 	}
-	
+
 	@Bean
-	public ReloadableResourceBundleMessageSource messageSource() {
-		String sep = File.separator;
-		String prefix = sep + "WEB-INF" + sep + "messages";
-		
-		try {
-			ServletContext servletContext = context.getBean(ServletContext.class);
-			File dir = new File(servletContext.getRealPath("/WEB-INF/messages/"));
-			
-			List<String> names = new ArrayList<String>();
-			if (dir.exists() && dir.listFiles() != null) {
-				for (File file : dir.listFiles()) {
-					if (file == null || !file.isFile()) continue;
-					String name = file.getName();
-					name = name.substring(0, name.lastIndexOf("."));
-					if (name.contains("_")) name = name.substring(0, name.indexOf("_"));
-					if (!names.contains(prefix + sep + name)) names.add(prefix + sep + name);
-				}
-			}
-			
-			ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-			messageSource.setDefaultEncoding("UTF-8");
-			if (names.size() > 0) {
-				String[] nameArray = names.toArray(new String[names.size()]);
-				messageSource.setBasenames(nameArray);
-				messageSource.setCacheSeconds(0);
-			}
-			
-			return messageSource;
-		}
-		catch(Exception ex) {
-			return null;
-		}
+	public ThreadPoolTaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(5);
+		taskExecutor.setMaxPoolSize(10);
+		taskExecutor.setQueueCapacity(25);
+		return taskExecutor;
 	}
 	
 	@Bean
